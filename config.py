da40af1efcae7a0839727ddff30660742fa9565d
Srivikram E <srivikram.e@gmail.com>
2026-05-31 14:32:34 +0530

    Update MySQL password in config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-fresh-basket-2024'
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'Srivikram@10'  # Update with your MySQL password
+    MYSQL_PASSWORD = 'Your_Password'  # Update with your MySQL password
     MYSQL_DB = 'fresh_basket'
     UPLOAD_FOLDER = os.path.join('static', 'img', 'products')
